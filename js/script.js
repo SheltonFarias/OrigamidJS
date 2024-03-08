4819f065c30c07de6c798aeafd14cb23cbb9db13
sheltonFarias <sheltonm10@outlook.com>
2024-03-08 16:46:04 -0300

    data object

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
-import * as teste from './modules/teste.js';
-import initModal from './modules/modal.js'
+import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
-
-teste.teste1()
-teste.teste2()
-console.log(teste.senha);
-
-console.log(this)
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
-initModal ();
+initModal();
 initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
