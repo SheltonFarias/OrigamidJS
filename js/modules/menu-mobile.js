00309c02f330ebf8d31203c5ad8184efbba6c5e7
sheltonFarias <sheltonm10@outlook.com>
2024-03-12 16:27:28 -0300

    correction

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,4 +1,4 @@
-import outsideClick from './outsideclick';
+import outsideClick from './outsideclick.js';
 
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
